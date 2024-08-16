163e180e4237a3b8678b5b1bb9d127cc66d440c2
RuleBritonnica <mbritton460@gmail.com>
2024-08-16 16:14:51 +1000

    Working on the CLI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+use clap::{Arg, Command};
+
+/*
+One potential design for the CLI is to take the following arguments:
+
+file_path:  The path to the file that contains just the code that will be refactored.
+target_var: The name of the variable/function/struct/impl to be refactored
+new_name:   The new name for the variable/function/... that is being refactored
+
+Optional arguments
+- out_path: If you want a new file to be created instead of just overwriting the original file
+
+
+*/
+
+mod messages;
+use crate::messages::{
+    about::ABOUT,
+    author::AUTHOR,
+    help::HELP,
+    version::VERSION,
+};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = Command::new("rem-cli")
+        .version(VERSION)
+        .author(AUTHOR)
+        .about(ABOUT)
+        .arg(
+            Arg::new("file_path")
+        )
+        .arg(
+            Arg::new("target_var")
+        )
+        .arg(
+            Arg::new("new_name")
+        )
+        .arg(
+            Arg::new("out_path")
+                .short('o')
+                .long("out"),
+        );
 }
---
+++ src/messages/about.rs
@@ -0,0 +1,10 @@
+pub const ABOUT: &str =
+r#"A CLI tool for the REM toolchain.
+Utilizes:
+    - rem-controller: git= https://github.com/RuleBrittonica/rem-controller
+    - rem-borrower:   git= https://github.com/RuleBrittonica/rem-borrower
+    - rem-repairer:   git= https://github.com/RuleBrittonica/rem-repairer
+    - rem-utils:      git= https://github.com/RuleBrittonica/rem-utils
+
+Designed and implemented for the rem-vscode plugin, available at git= https://github.com/RuleBrittonica/rem-vscode
+"#;
